7d6e70cff3b108216368bceaa16ebc278bf9d0eb
licunzhi <licunzhi@trendytech.com.cn>
2018-03-12 08:59:44 +0800

    Swagger实现API接口

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,24 @@
 package com.example.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ *
+ * @ComponentScan
+ * 这个注解在Spring中很重要，它对应XML配置中的元素，@ComponentScan的功能其实就是自动扫描并加载符合条件的组件
+ * （比如@Component和@Repository等）或者bean定义，最终将这些bean定义加载到IoC容器中。
+ * 我们可以通过basePackages等属性来细粒度的定制@ComponentScan自动扫描的范围，
+ * 如果不指定，则默认Spring框架实现会从声明@ComponentScan所在类的package进行扫描。
+ * 注：所以SpringBoot的启动类最好是放在root package下，因为默认不指定basePackages。
+ *
+ *
+ */
 @SpringBootApplication
-@MapperScan("com.example.demo.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/example/demo/vo/User.java
@@ -0,0 +1,54 @@
+package com.example.demo.vo;
+
+/**
+ * @author licunzhi
+ * @desc 描述功能
+ * @date 2018-03-07
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
